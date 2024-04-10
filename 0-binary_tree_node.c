828712189c2b352ff5774446e7bbbedec0c9b862
Your Name <you@example.com>
2024-04-10 18:41:54 +0000

    Syntax clean up in 0-binary_tree file

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,24 +1,6 @@
 #include <stdlib.h>
 
 /**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
-struct binary_tree_s
-{
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-};
-
-typedef struct binary_tree_s binary_tree_t;
-
-/**
  * binary_tree_node - Creates a binary tree node
  *@parent: pointer to the parent node of the node to create
  *@value: value to insert into the node
@@ -31,7 +13,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
   if (new_node == NULL)
     return (NULL);
 
-      new_node->n = value;
+  new_node->n = value;
   new_node->parent = parent;
   new_node->left = NULL;
   new_node->right = NULL;
