1eb711be7a8ba6df254a295aa9373c6dd3d68bd5
Your Name <you@example.com>
2024-04-10 18:16:37 +0000

    Added data struct to file

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,5 +1,21 @@
 #include <stdlib.h>
 
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
+
 typedef struct binary_tree_s binary_tree_t;
 
 /**
@@ -13,7 +29,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
   binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
   if (new_node == NULL)
-    return (NULL):
+    return (NULL);
 
       new_node->n = value;
   new_node->parent = parent;
