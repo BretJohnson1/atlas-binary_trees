fc1abb22ec6cc2833acae5673966a2b5196ff0c5
BretJohnson1 <bretjohnson004@gmail.com>
2024-04-15 16:14:59 +0000

    Reworking 9-binary file

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,14 +7,19 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t height_left = 0, height_right = 0;
 
-	if (tree == NULL)
-		return (-1);
+	if (!tree)
+		return (0);
 
-	left_height = binary_tree_height(tree->left);
+	if (tree->left)
+		height_left = 1 + binary_tree_height(tree->left);
 
-	right_height = binary_tree_height(tree->right);
+	if (tree->right)
+		height_right = 1 + binary_tree_height(tree->right);
 
-	return (left_height > right_height ? left_height : right_height) + 1;
+	if (height_left > height_right)
+		return (height_left);
+	else
+		return (height_right);
 }
