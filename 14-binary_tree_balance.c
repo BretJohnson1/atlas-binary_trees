8882d69df3b57b6d5725a6e6d68092b1acafd438
BretJohnson1 <bretjohnson004@gmail.com>
2024-04-15 16:08:00 +0000

    Created 14-binary file

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_balance - measures the balance of a tree
+ *@tree: pointer to the root node
+ *
+ *Return: balance factor of tree or 0 if NULL
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+	
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return ((int)(left_height - right_height));
+}
