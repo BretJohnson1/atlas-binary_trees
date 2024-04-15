31236230b0eef75dffea0e653c3d8d5d81397277
BretJohnson1 <bretjohnson004@gmail.com>
2024-04-15 15:45:14 +0000

    Created 11-binary file

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size - maesures the size of a tree
+ *@tree: pointer to the root node
+ *
+ *Return: size of the tree or 0 if NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	size_t left_size = binary_tree_size(tree->left);
+	size_t right_size = binary_tree_size(tree->right);
+
+	return (left_size + right_size + 1);
+}
